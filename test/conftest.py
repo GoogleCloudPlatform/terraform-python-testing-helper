aa261fc96c43e4518de6b69d1a6645a246530b03
Ludovico Magnocavallo <ludomagno@google.com>
2019-10-29 00:37:35 +0100

    cleanup module interface, remove old tests, new plan output test

--- test/conftest.py
+++ test/conftest.py
@@ -11,3 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+import pytest
+
+
+@pytest.fixture(scope='session')
+def fixtures_dir():
+  return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures')
