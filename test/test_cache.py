041f476569a050dae8a199ceb0ad33492f204fd7
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-10-31 16:23:27 -0700

    fix pep8 indenting

--- test/test_cache.py
+++ test/test_cache.py
@@ -32,31 +32,31 @@ cache_methods = ["setup", "init", "plan", "apply", "output", "destroy"]
 
 @pytest.fixture
 def tf(request, fixtures_dir):
-    terra = tftest.TerraformTest(
-        tfdir='plan_no_resource_changes',
-        basedir=fixtures_dir,
-        enable_cache=request.param,
-    )
-    yield terra
+  terra = tftest.TerraformTest(
+      tfdir='plan_no_resource_changes',
+      basedir=fixtures_dir,
+      enable_cache=request.param,
+  )
+  yield terra
 
-    _LOGGER.debug("Removing cache dir")
-    try:
-        shutil.rmtree(terra.cache_dir)
-    except FileNotFoundError:
-        _LOGGER.debug("%s does not exists", terra.cache_dir)
+  _LOGGER.debug("Removing cache dir")
+  try:
+    shutil.rmtree(terra.cache_dir)
+  except FileNotFoundError:
+    _LOGGER.debug("%s does not exists", terra.cache_dir)
 
 
 @pytest.mark.parametrize("tf", [True], indirect=True)
 def test_use_cache(tf):
-    """
-    Ensures cache is used and runs the execute_command() for first call of the 
-    method only
-    """
-    for method in cache_methods:
-        with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-            for _ in range(2):
-                getattr(tf, method)(use_cache=True)
-            assert mock_execute_command.call_count == 1
+  """
+  Ensures cache is used and runs the execute_command() for first call of the 
+  method only
+  """
+  for method in cache_methods:
+    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+      for _ in range(2):
+        getattr(tf, method)(use_cache=True)
+      assert mock_execute_command.call_count == 1
 
 
 @pytest.mark.parametrize("tf", [
@@ -70,109 +70,109 @@ def test_use_cache(tf):
     ),
 ], indirect=True)
 def test_no_use_cache(tf):
-    """
-    Ensures cache is not used and runs the execute_command() for every call of
-    the method
-    """
-    expected_call_count = 2
-    for method in cache_methods:
-        with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-            for _ in range(expected_call_count):
-                getattr(tf, method)(use_cache=False)
-            assert mock_execute_command.call_count == expected_call_count
+  """
+  Ensures cache is not used and runs the execute_command() for every call of
+  the method
+  """
+  expected_call_count = 2
+  for method in cache_methods:
+    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+      for _ in range(expected_call_count):
+        getattr(tf, method)(use_cache=False)
+      assert mock_execute_command.call_count == expected_call_count
 
 
 @pytest.mark.parametrize("tf", [True], indirect=True)
 def test_use_cache_with_same_tf_var_file(tf, tmp_path):
-    tf_var_file_methods = ["plan", "apply", "destroy"]
+  tf_var_file_methods = ["plan", "apply", "destroy"]
 
-    tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
-    tf_vars_file.write_text(json.dumps({"foo": "old"}))
+  tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
+  tf_vars_file.write_text(json.dumps({"foo": "old"}))
 
-    for method in tf_var_file_methods:
-        with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-            for _ in range(2):
-                getattr(tf, method)(use_cache=True, tf_var_file=tf_vars_file)
+  for method in tf_var_file_methods:
+    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+      for _ in range(2):
+        getattr(tf, method)(use_cache=True, tf_var_file=tf_vars_file)
 
-        assert mock_execute_command.call_count == 1
+    assert mock_execute_command.call_count == 1
 
 
 @pytest.mark.parametrize("tf", [True], indirect=True)
 def test_use_cache_with_new_tf_var_file(tf, tmp_path):
-    tf_var_file_methods = ["plan", "apply", "destroy"]
-    expected_call_count = 2
+  tf_var_file_methods = ["plan", "apply", "destroy"]
+  expected_call_count = 2
 
-    tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
+  tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
 
-    for method in tf_var_file_methods:
-        tf_vars_file.write_text(json.dumps({"foo": "old"}))
-        with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-            for _ in range(expected_call_count):
-                getattr(tf, method)(use_cache=True, tf_var_file=tf_vars_file)
-                tf_vars_file.write_text(json.dumps({"foo": "new"}))
+  for method in tf_var_file_methods:
+    tf_vars_file.write_text(json.dumps({"foo": "old"}))
+    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+      for _ in range(expected_call_count):
+        getattr(tf, method)(use_cache=True, tf_var_file=tf_vars_file)
+        tf_vars_file.write_text(json.dumps({"foo": "new"}))
 
-        assert mock_execute_command.call_count == expected_call_count
+    assert mock_execute_command.call_count == expected_call_count
 
 
 @pytest.mark.parametrize("tf", [True], indirect=True)
 def test_use_cache_with_new_extra_files(tf, tmp_path):
-    expected_call_count = 2
-    tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
-    tf_vars_file.write_text(json.dumps({"foo": "old"}))
+  expected_call_count = 2
+  tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
+  tf_vars_file.write_text(json.dumps({"foo": "old"}))
 
-    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-        for _ in range(expected_call_count):
-            tf.setup(use_cache=True, extra_files=[tf_vars_file])
-            tf_vars_file.write_text(json.dumps({"foo": "new"}))
+  with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+    for _ in range(expected_call_count):
+      tf.setup(use_cache=True, extra_files=[tf_vars_file])
+      tf_vars_file.write_text(json.dumps({"foo": "new"}))
 
-        assert mock_execute_command.call_count == expected_call_count
+    assert mock_execute_command.call_count == expected_call_count
 
 
 @pytest.mark.parametrize("tf", [True], indirect=True)
 def test_use_cache_with_same_extra_files(tf, tmp_path):
-    tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
-    tf_vars_file.write_text(json.dumps({"foo": "old"}))
+  tf_vars_file = tmp_path / (str(uuid.uuid4()) + '.json')
+  tf_vars_file.write_text(json.dumps({"foo": "old"}))
 
-    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-        for _ in range(2):
-            tf.setup(use_cache=True, extra_files=[tf_vars_file])
+  with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+    for _ in range(2):
+      tf.setup(use_cache=True, extra_files=[tf_vars_file])
 
-        assert mock_execute_command.call_count == 1
+    assert mock_execute_command.call_count == 1
 
 
 @pytest.mark.parametrize("tf", [True], indirect=True)
 def test_use_cache_with_new_env(tf):
-    expected_call_count = 2
-    for method in cache_methods:
-        with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-            for _ in range(expected_call_count):
-                getattr(tf, method)(use_cache=True)
-                tf._env["foo"] = "bar"
+  expected_call_count = 2
+  for method in cache_methods:
+    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+      for _ in range(expected_call_count):
+        getattr(tf, method)(use_cache=True)
+        tf._env["foo"] = "bar"
 
-            assert mock_execute_command.call_count == expected_call_count
+      assert mock_execute_command.call_count == expected_call_count
 
-            del tf._env["foo"]
+      del tf._env["foo"]
 
 
 @pytest.fixture
 def dummy_tf_filepath(tf):
-    filepath = os.path.join(tf.tfdir, "bar.txt")
-    with open(filepath, "w") as f:
-        f.write("old")
+  filepath = os.path.join(tf.tfdir, "bar.txt")
+  with open(filepath, "w") as f:
+    f.write("old")
 
-    yield filepath
+  yield filepath
 
-    os.remove(filepath)
+  os.remove(filepath)
 
 
 @pytest.mark.parametrize("tf", [True], indirect=True)
 def test_use_cache_with_new_tf_content(tf, dummy_tf_filepath):
-    expected_call_count = 2
-    for method in cache_methods:
-        with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
-            for _ in range(expected_call_count):
-                getattr(tf, method)(use_cache=True)
-                with open(dummy_tf_filepath, "w") as f:
-                    f.write(str(uuid.uuid4()))
+  expected_call_count = 2
+  for method in cache_methods:
+    with patch.object(tf, 'execute_command', wraps=tf.execute_command) as mock_execute_command:
+      for _ in range(expected_call_count):
+        getattr(tf, method)(use_cache=True)
+        with open(dummy_tf_filepath, "w") as f:
+          f.write(str(uuid.uuid4()))
 
-            assert mock_execute_command.call_count == expected_call_count
+      assert mock_execute_command.call_count == expected_call_count
